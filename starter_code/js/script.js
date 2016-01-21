dc4a75055099fa06b53d347e79225421179b4729
Sheng0o0 <shenghaozheng@mybihs.org>
2016-01-21 22:58:56 +0000

    flickr search

--- starter_code/js/script.js
+++ starter_code/js/script.js
@@ -1,1 +1,21 @@
 // YOUR CODE GOES HERE
+function callback(p1){
+    // console.log(p1)
+    for (var i = 0; i < p1.length ; i++){
+    $('#search-results').append("<img src=>");
+    $('#search-results').append(p1[i].media.m);
+    }
+    // console.log(p1[0].media.m);
+}
+
+
+function flickrSearch(){
+    var search = $('#search-query').val();
+    searchFlickr(search,callback) ;
+   
+}
+$(document).ready(function(){
+
+  $('#search-btn').click(flickrSearch);
+    
+})
